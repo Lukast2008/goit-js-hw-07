696e82a1022473f21019fde95f3a15324a4e1d85
Lukast2008 <lukast2008@gmail.com>
2022-10-26 14:23:03 +0300

    srthw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryAllPhotoPreview = document.querySelector(".gallery");
+let mass = [];
+
+for (let i = 0; i < galleryItems.length; i += 1) {
+  const newImg = document.createElement("img");
+  const { preview } = galleryItems[i];
+  console.log(preview);
+  newImg.src = preview;
+  newImg.style.width = "270px";
+  newImg.classList.add = "gallery__image";
+  //   newImg = ;
+
+  mass.push(newImg);
+}
+
+galleryAllPhotoPreview.append(...mass);
