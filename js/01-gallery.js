0cbc1fcd9894cddbf0e893d71ba2d8c80e4a46a1
Lukast2008 <lukast2008@gmail.com>
2022-10-26 21:16:15 +0300

    first hw1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,101 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-console.log(galleryItems);
 
 const galleryAllPhotoPreview = document.querySelector(".gallery");
-let mass = [];
 
-for (let i = 0; i < galleryItems.length; i += 1) {
-  const newImg = document.createElement("img");
-  const { preview } = galleryItems[i];
-  console.log(preview);
-  newImg.src = preview;
-  newImg.style.width = "270px";
-  newImg.classList.add = "gallery__image";
-  //   newImg = ;
+const gelImg = galleryItems
+  .map(
+    (el) =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href="${el.original}">
+        <img
+          class="gallery__image"
+          src=${el.preview}
+          data-source=${el.original}
+          alt=${el.description}
+        />
+      </a>
+    </div>`
+  )
+  .join("");
 
-  mass.push(newImg);
+galleryAllPhotoPreview.innerHTML = gelImg;
+
+galleryAllPhotoPreview.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img  src=${event.target.dataset.source}  width ="800" />`
+  );
+
+  instance.show();
+
+  document.addEventListener("keydown", keyEsc);
+  function keyEsc(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
 }
 
-galleryAllPhotoPreview.append(...mass);
+//%%%%%%%%%%%%%%%%%%
+
+// Change code below this line
+
+// console.log(galleryItems);
+
+// const galleryAllPhotoPreview = document.querySelector(".gallery");
+
+// const currentImgPaste = document.querySelector(".gallery__item");
+// const currentImg = document.querySelector(".gallery__image");
+// const imgLink = document.querySelector(".gallery__link");
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+
+//           <div class="gallery__item">
+//             <a class="gallery__link" href="large-image.jpg">
+//               <img class="gallery__image" src="small-image.jpg" data-source="large-image.jpg" alt="Image description" />
+//             </a>
+//           </div>
+
+//     </div>
+// `);
+
+// let mass = [];
+
+// let clickerImage;
+
+// for (let i = 0; i < galleryItems.length; i += 1) {
+//   const newImg = document.createElement("img");
+//   const { preview, original, description } = galleryItems[i];
+//   newImg.src = preview;
+//   newImg.setAttribute("alt", description);
+//   newImg.setAttribute("data-source", original);
+//   newImg.style.width = "370px";
+//   newImg.classList.add = "gallery__image";
+//   mass.push(newImg);
+// }
+// galleryAllPhotoPreview.append(...mass);
+
+// galleryAllPhotoPreview.addEventListener("click", handlerImg);
+
+// function handlerImg(event) {
+//   event.preventDefault();
+
+//   instance.show();
+//   currentImg.src = event.target.getAttribute(["data-source"]);
+// }
+
+// document.addEventListener("keydown", keyEsc);
+// function keyEsc(event) {
+//   event.preventDefault();
+
+//   if (event.key === "Escape") {
+//     instance.close();
+//   }
+// }
