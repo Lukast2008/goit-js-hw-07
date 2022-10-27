879b87b5e2e5226b12c118de29c056b3dd57bdad
Lukast2008 <lukast2008@gmail.com>
2022-10-27 17:25:24 +0300

    HW7 Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryAllPhotoPreview = document.querySelector(".gallery");
+
+const gelImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+    </a>`
+  )
+  .join("");
+
+galleryAllPhotoPreview.innerHTML = gelImg;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
